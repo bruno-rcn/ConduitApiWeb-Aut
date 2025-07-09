08d3de6041e429b8331ebaec470bfd0255ac6087
Bruno <brunorcnoberto@hotmail.com>
2025-07-09 09:14:56 +0000

    update project

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,22 +5,18 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 1,
   reporter: 'html',
   use: {
     trace: 'on-first-retry',
-    baseURL: 'https://conduit.bondaracademy.com/'
+    baseURL: 'https://conduit.bondaracademy.com/',
+    screenshot: 'on'
   },
 
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
     },
 
     {
