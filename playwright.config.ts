2bc9ba3074493f8b70dfbf3e459fc48cfec71059
Bruno <brunorcnoberto@hotmail.com>
2025-07-09 09:37:11 +0000

    update

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : 1,
-  reporter: 'html',
+  reporter: [["line"], ["allure-playwright"], ['html']],
   use: {
     trace: 'on-first-retry',
     baseURL: 'https://conduit.bondaracademy.com/',
